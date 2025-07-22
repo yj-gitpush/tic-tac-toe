68ea135f0cc7a20e2060b32e0452b7a9ffae39f0
Yash Jain <yash.jain@zorang.com>
2025-07-23 00:18:37 +0530

    first commit

---
+++ src/test/java/com/lld/tictactoe/TictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lld.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
